e3362b683969208610d1120704306bcd5382087a
Dmytro Yarmak <dmytroyarmak@gmail.com>
2017-12-28 07:28:39 +0200

    Add unit tests for AppComponent#ngOnInit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,34 @@
 import { AppComponent } from './app.component';
+import { of as observableOf } from 'rxjs/observable/of';
 
 describe('AppComponent', () => {
+  let appComponent;
+  let rijksmuseumApiServiceMock;
+  let getAllResponseMock;
+
+  beforeEach(() => {
+    rijksmuseumApiServiceMock = jasmine.createSpyObj('rijksmuseumApiService', [
+      'getAll'
+    ]);
+    getAllResponseMock = {
+      artObjects: 'artObjectsMock'
+    };
+    rijksmuseumApiServiceMock.getAll.and.returnValue(observableOf(getAllResponseMock));
+
+    appComponent = new AppComponent(rijksmuseumApiServiceMock);
+  });
+
+  describe('#ngOnInit', () => {
+    beforeEach(() => {
+      appComponent.ngOnInit();
+    });
+
+    it('should call getAll on rijksmuseumApiService with empty search query', () => {
+      expect(rijksmuseumApiServiceMock.getAll).toHaveBeenCalledWith('');
+    });
+
+    it('should store artObjects from response', () => {
+      expect(appComponent.artObjects).toBe(getAllResponseMock.artObjects);
+    });
+  });
 });
