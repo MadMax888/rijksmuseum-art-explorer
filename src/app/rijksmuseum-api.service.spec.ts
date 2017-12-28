560d1cf5aed4deb57256ab38fe56a500a32e8993
Dmytro Yarmak <dmytroyarmak@gmail.com>
2017-12-28 07:10:31 +0200

    Remove generated specs

--- src/app/rijksmuseum-api.service.spec.ts
+++ src/app/rijksmuseum-api.service.spec.ts
@@ -1,15 +1,4 @@
-import { TestBed, inject } from '@angular/core/testing';
-
 import { RijksmuseumApiService } from './rijksmuseum-api.service';
 
 describe('RijksmuseumApiService', () => {
-  beforeEach(() => {
-    TestBed.configureTestingModule({
-      providers: [RijksmuseumApiService]
-    });
-  });
-
-  it('should be created', inject([RijksmuseumApiService], (service: RijksmuseumApiService) => {
-    expect(service).toBeTruthy();
-  }));
 });
